b426cc266bd2e7a865098b642588e2a71c433089
Ovanes <o-zakharyan@mail.ru>
2021-02-14 19:22:02 +0300

    the end of first lesson

--- VKapp/VKapp/LogInViewController.swift
+++ VKapp/VKapp/LogInViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class LogInViewController: UIViewController {
+    
+    // MARK: - Properties
 
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        passwordTextField.isSecureTextEntry = true
     }
 
-
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        print("login \(loginTextField.text ?? "")")
+        print("password \(passwordTextField.text ?? "")")
+    }
+    
 }
 
