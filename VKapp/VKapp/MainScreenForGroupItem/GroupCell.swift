e96d2fd6cc4bc71f3a33f95ec48bc09d0406c56d
Ovanes <o-zakharyan@mail.ru>
2021-02-27 20:34:13 +0300

    add groups cell

---
+++ VKapp/VKapp/MainScreenForGroupItem/GroupCell.swift
@@ -0,0 +1,15 @@
+//
+//  GroupCell.swift
+//  VKapp
+//
+//  Created by Ovanes on 25.02.2021.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+
+    @IBOutlet var groupsLabel: UILabel!
+    
+
+}
---
+++ VKapp/VKapp/MainScreenForGroupItem/Groups.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  VKapp
+//
+//  Created by Ovanes on 26.02.2021.
+//
+
+import Foundation
+
+
+struct Groups {
+    let name: String
+}
