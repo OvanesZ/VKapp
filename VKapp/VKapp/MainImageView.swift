67f29ee0890887494dd45d07b699b8cbdf449c7b
Ovanes <o-zakharyan@mail.ru>
2021-03-13 01:11:06 +0300

    add gradient class

--- VKapp/VKapp/MainImageView.swift
+++ VKapp/VKapp/MainImageView.swift
@@ -10,16 +10,24 @@ import UIKit
 class MainImageView: UIView {
 
     @IBOutlet var mainImageFriend: UIImageView!
+    @IBOutlet var mainImageShadowFriend: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         mainImageFriend.layer.cornerRadius = 16
         mainImageFriend.layer.masksToBounds = true
-        mainImageFriend.layer.shadowColor = UIColor.black.cgColor
-        mainImageFriend.layer.shadowRadius = 10
-        mainImageFriend.layer.shadowOpacity = 0.5
-        mainImageFriend.layer.shadowOffset = CGSize(width: 20, height: 10)
+      
         
+        
+        mainImageShadowFriend.layer.shadowColor = UIColor.black.cgColor
+        mainImageShadowFriend.layer.shadowRadius = 10
+        mainImageShadowFriend.layer.shadowOpacity = 0.5
+        mainImageShadowFriend.layer.shadowOffset = CGSize(width: 20, height: 10)
+        mainImageShadowFriend.layer.masksToBounds = false
+        mainImageShadowFriend.backgroundColor != .clear
+      
+        
+       
  
     }
   
